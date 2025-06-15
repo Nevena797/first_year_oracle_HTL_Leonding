8c6a90a91421e5f452b1a306e323eced10ce3a58
Nevena <nevena.rogova@gmx.at>
2025-06-15 22:54:38 +0300

    Update Text_function.sql

--- Text_function.sql
+++ Text_function.sql
@@ -54,3 +54,63 @@ FROM (
 )
 WHERE NACHNAME LIKE '%e%';
 
+SELECT
+    NAME,
+    INSTR(NAME, 'a'),-- an welcher Stelle der Buchstabe erstmals vorkommt
+    INSTR(NAME, 'o', 3)-- Suche nach der 3. Stelle 
+FROM PLAYERS;
+
+SELECT
+    NAME,
+    RPAD(name, 20, '*'),
+    RPAD(name, 20, '*')
+FROM
+    PLAYERS;
+
+SELECT
+    TRIM('P' FROM NAME)
+    --TRIM('Pa' FROM NAME)/*do not work so*/
+FROM
+    PLAYERS;
+    
+SELECT TRIM('P' FROM NAME) FROM PLAYERS;
+
+SELECT NAME,
+    '|' || NAME || '|',
+    '|' || LTRIM(NAME) || '|',
+    '|' || RTRIM(NAME) || '|',
+    '|' || TRIM(NAME) || '|'
+FROM
+(SELECT ' ' || NAME || ' ' NAME FROM PLAYERS);
+
+SELECT TRIM('P' from name) FROM PLAYERS; /* Nur ein Zeichen moeglich*/
+SELECT NAME FROM PLAYERS;
+SELECT NAME, REPLACE(NAME, 'C', 'K') FROM PLAYERS;
+
+SELECT 45.926, ROUND(45.926), TRUNC(45.926) FROM DUAL;
+
+SELECT
+    5,
+    5/4,
+    MOD(9,4)
+FROM DUAL;
+
+SELECT
+    SYSDATE
+FROM
+    DUAL
+WHERE
+    SYSDATE = TO_DATE('15.06.25', 'DD.MM.YY');
+    
+SELECT SYSDATE
+FROM DUAL
+WHERE TRUNC(SYSDATE) = TO_DATE('15.06.25', 'DD.MM.YY');
+    
+SELECT
+    SYSDATE,
+    TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS'),
+    TO_CHAR(TO_DATE('15.06.25', 'DD.MM.YY'), 'YYYY-MM-DD HH24:MI:SS')
+FROM
+    DUAL; 
+    
+
